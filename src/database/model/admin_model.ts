b4240377b1ff4ef7c08c214d327eb6e36df174d3
janjao937 <janjao937@gmail.com>
2025-05-12 19:45:12 +0700

    admin model

---
+++ src/database/model/admin_model.ts
@@ -0,0 +1,23 @@
+import { Db_Model } from "../db";
+
+const tableName = 'admin';
+const adminModel = `CREATE TABLE ${tableName} (
+id VARCHAR(36) DEFAULT (UUID()) NOT NULL UNIQUE PRIMARY KEY,
+role VARCHAR(255) NOT NULL,
+name VARCHAR(255) NOT NULL,
+lastname VARCHAR(255) NOT NULL,
+username VARCHAR(255) NOT NULL,
+phone_number VARCHAR(12) NOT NULL
+)`;
+
+export default { tableName, model: adminModel } as Db_Model;
+
+interface admin_Model {
+  id: number;
+  role: string; // may change to enum
+  username: string;
+  password: string;
+  first_name: string;
+  last_name: string;
+  phone_number: string;
+}
